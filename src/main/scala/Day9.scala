c7e8cf8bd7ddd55dfaaf703442317dd8d844e97a
Michael Reitgruber <reitgruber.michael@gmail.com>
2022-12-09 14:58:38 +0100

    Day 9

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object Day9:
   def main(args: Array[String]): Unit =
-    val input = Util.loadDayLines(9)
+    val input = Util.loadDayLines(9).flatMap{ case s"$dir $count" => List.fill(count.toInt)(dir) }
 
     //Part 1
     println(move(input, 1).size)
@@ -16,22 +16,12 @@ object Day9:
     @tailrec
     def moveH(head: (Int, Int), tail: List[(Int, Int)], moves: List[String], visited: Set[(Int, Int)]): Set[(Int, Int)] = moves match
       case x :: xs =>
-        val split = x.split(" ")
-        val direction = split(0)
-        val count = split(1).toInt
-
-        val newHead = direction match
+        val newHead = x match
           case "R" => (head._1, head._2 + 1)
           case "L" => (head._1, head._2 - 1)
           case "U" => (head._1 + 1, head._2)
           case "D" => (head._1 - 1, head._2)
-
-        val newTail = step(newHead, tail)
-
-        if count == 1 then
-          moveH(newHead, newTail, xs, visited + tail.last)
-        else
-          moveH(newHead, newTail, xs.prepended(direction + " " + (count - 1)), visited + tail.last)
+        moveH(newHead, step(newHead, tail), xs, visited + tail.last)
       case _ => visited + tail.last
     moveH((0, 0), List.fill(tailSize)((0, 0)), moves, Set())
 
@@ -42,14 +32,7 @@ object Day9:
       val yDist = front._2 - next._2
       val newElem =
         if Math.abs(xDist) > 1 || Math.abs(yDist) > 1 then
-          if front._1 != next._1 && front._2 != next._2 then
-            (next._1 + xDist.sign, next._2 + yDist.sign)
-          else if front._1 != next._1 then
-            (next._1 + xDist.sign, next._2)
-          else if front._2 != next._2 then
-            (next._1, next._2 + yDist.sign)
-          else
-            next
+          (next._1 + xDist.sign, next._2 + yDist.sign)
         else
           next
       step(newElem, rest, newTail.appended(newElem))
