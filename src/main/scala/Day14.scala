30a05d8e5adc8dbda1cfa3ba25dc64b15db0d1a5
Michael Reitgruber <reitgruber.michael@gmail.com>
2022-12-15 20:56:56 +0100

    Improved performance Day14

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -4,18 +4,18 @@ import scala.annotation.tailrec
 
 object Day14 extends Day(14):
   override def solve(): Unit =
-    val cave: Map[(Int, Int), Char] = parse(inputLines)
+    val blocked: Set[(Int, Int)] = parse(inputLines)
 
     //Part 1
-    println(countDrops(cave, dropsToInfinity))
+    println(countDrops(blocked, dropsToInfinity, isFree))
 
     //Part 2
-    println(countDrops(cave.withDefault(k => defaultWithFloor(k, cave.keys.maxBy(_._2)._2 + 2)), sourceBlocked))
+    println(countDrops(blocked, sourceBlocked, isFreeWithFloor(blocked.maxBy(_._2)._2 + 2)))
 
   @tailrec
-  def parse(input: List[String], acc: Map[(Int, Int), Char] = Map().withDefaultValue(' ')): Map[(Int, Int), Char] = input match
+  def parse(input: List[String], acc: Set[(Int, Int)] = Set()): Set[(Int, Int)] = input match
     case x :: xs =>
-      parse(xs, acc ++ x.split(" -> ").map(_.split(",")).map(s => (s.head.toInt, s(1).toInt)).sliding(2).flatMap(s => drawLine(s.head, s(1))).map(c => c -> '#').toMap)
+      parse(xs, acc ++ x.split(" -> ").map(_.split(",")).map(s => (s.head.toInt, s(1).toInt)).sliding(2).flatMap(s => drawLine(s.head, s(1))))
     case _ => acc
 
   def drawLine(start: (Int, Int), end: (Int, Int)): Set[(Int, Int)] =
@@ -27,28 +27,30 @@ object Day14 extends Day(14):
       (s to e).map(x => (x, start._2)).toSet
 
   @tailrec
-  def countDrops(cave: Map[(Int, Int), Char], stopCondition: (Map[(Int, Int), Char], (Int, Int)) => Boolean, acc: Int = 0): Int = dropSand(cave, stopCondition) match
-    case (true, newCave) => countDrops(newCave, stopCondition, acc + 1)
+  def countDrops(blocked: Set[(Int, Int)], stopCondition: (Set[(Int, Int)], (Int, Int)) => Boolean, free: (Set[(Int, Int)], (Int, Int)) => Boolean, acc: Int = 0): Int = dropSand(blocked, stopCondition, free) match
+    case (true, newBlocked) => countDrops(newBlocked, stopCondition, free, acc + 1)
     case (false, _) => acc
 
-  def dropSand(cave: Map[(Int, Int), Char], stopCondition: (Map[(Int, Int), Char], (Int, Int)) => Boolean): (Boolean, Map[(Int, Int), Char]) =
+  def dropSand(blocked: Set[(Int, Int)], stopCondition: (Set[(Int, Int)], (Int, Int)) => Boolean, free: (Set[(Int, Int)], (Int, Int)) => Boolean): (Boolean, Set[(Int, Int)]) =
     @tailrec
-    def dropSandH(sand: (Int, Int)): (Boolean, Map[(Int, Int), Char]) =
-      if stopCondition(cave, sand) then
-        (false, cave)
-      else if cave((sand._1, sand._2 + 1)) == ' ' then
+    def dropSandH(sand: (Int, Int)): (Boolean, Set[(Int, Int)]) =
+      if stopCondition(blocked, sand) then
+        (false, blocked)
+      else if free(blocked, (sand._1, sand._2 + 1)) then
         dropSandH((sand._1, sand._2 + 1))
-      else if cave((sand._1 - 1, sand._2 + 1)) == ' ' then
+      else if free(blocked, (sand._1 - 1, sand._2 + 1)) then
         dropSandH((sand._1 - 1, sand._2 + 1))
-      else if cave((sand._1 + 1, sand._2 + 1)) == ' ' then
+      else if free(blocked, (sand._1 + 1, sand._2 + 1)) then
         dropSandH((sand._1 + 1, sand._2 + 1))
       else
-        (true, cave + (sand -> '+'))
+        (true, blocked + sand)
 
     dropSandH((500, 0))
 
-  def defaultWithFloor(key: (Int, Int), floor: Int): Char = if key._2 >= floor then '#' else ' '
+  def isFree(blocked: Set[(Int, Int)], key: (Int, Int)): Boolean = !blocked.contains(key)
 
-  def dropsToInfinity(cave: Map[(Int, Int), Char], sand: (Int, Int)): Boolean = sand._2 > cave.keys.maxBy(_._2)._2
+  def isFreeWithFloor(floor: Int)(blocked: Set[(Int, Int)], key: (Int, Int)): Boolean = if key._2 >= floor then false else !blocked.contains(key)
 
-  def sourceBlocked(cave: Map[(Int, Int), Char], sand: (Int, Int)): Boolean = cave((500, 0)) != ' '
+  def dropsToInfinity(blocked: Set[(Int, Int)], sand: (Int, Int)): Boolean = sand._2 > blocked.maxBy(_._2)._2
+
+  def sourceBlocked(blocked: Set[(Int, Int)], sand: (Int, Int)): Boolean = blocked.contains((500, 0))
