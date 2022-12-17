40438b08b10a237943da56c2c9e8aece49d4328b
Michael Reitgruber <reitgruber.michael@gmail.com>
2022-12-17 18:52:08 +0100

    d16 working, but too slow

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -2,7 +2,6 @@ import util.{Day, Util}
 
 import scala.annotation.tailrec
 
-
 object Day16 extends Day(16):
   override def solve(): Unit =
     val testInput = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -15,74 +14,91 @@ object Day16 extends Day(16):
                       |Valve HH has flow rate=22; tunnel leads to valve GG
                       |Valve II has flow rate=0; tunnels lead to valves AA, JJ
                       |Valve JJ has flow rate=21; tunnel leads to valve II""".stripMargin.split("\n").map(_.strip).toList
-/*
+
+    val (rates, edges) = parse(testInput)
+    val graph = simplify(rates, edges.groupMap(_._1)(_._2))
+
+    //println(maxFlow(rates, graph))
+
     Util.time {
-      println(maxRelease(parse(inputLines)))
-    }
-    */
-    Util.time {
-      println(maxReleaseElephant(parse(testInput)))
+      println(maxFlowE(rates, graph))
     }
 
-  @tailrec
-  def parse(lines: List[String], acc: Map[String, (Int, List[String])] = Map()): Map[String, (Int, List[String])] = lines match
-    case x :: xs =>
-      val (v, r, n) = x match {
-        case s"Valve $name has flow rate=$rate; tunnels lead to valves $leads" => (name, rate.toInt, leads.split(", ").toList)
-        case s"Valve $name has flow rate=$rate; tunnel leads to valve $leads" => (name, rate.toInt, List(leads))
-      }
-      parse(xs, acc + (v -> (r, n)))
-    case _ => acc
 
-  def maxRelease(valves: Map[String, (Int, List[String])]): Int =
-    var globalMax = 0
-    val t = 30
-    lazy val maxReleaseH: ((String, Int, Set[String], Int)) =>  Int = Util.memoize {
-      case (curr, currFlow, opened, time) =>
-        if time == t then
-          if currFlow > globalMax then
-            globalMax = currFlow
-          currFlow
-        else if opened.contains(curr) || valves(curr)._1 == 0 then
-          val rem = valves.keys.filter(k => !opened.contains(k)).map(k => valves(k)._1).sum * Math.max(t - time - 1, 0)
-          if (currFlow + rem) < globalMax then
-            currFlow
-          else
-            valves(curr)._2.map(n => maxReleaseH(n, currFlow, opened, time + 1)).max
+  def simplify(rates: Map[String, Int], edges: Map[String, Set[String]]): Map[String, Set[(String, Int)]] =
+    val relevantStarts = rates.filter(k => k._1 == "AA" || k._2 != 0)
+    val relevantEnds = relevantStarts.filter(k => k._1 != "AA")
+
+    relevantStarts.map(s =>
+      (s._1, relevantEnds.filter(_._1 != s._1).map(e => (e._1, Util.bfs(s._1, e._1, edges.apply.andThen(_.toList)).distance)).toSet)
+    )
+
+  def parse(l: List[String]): (Map[String, Int], Set[(String, String)]) = l.foldLeft(Map[String, Int](), Set[(String, String)]())((acc, x) =>
+    x match {
+      case s"Valve $name has flow rate=$rate; tunnels lead to valves $n" =>
+        val edges = n.split(", ").flatMap(e => List((name, e), (e, name))).toSet
+        (acc._1 + (name -> rate.toInt), acc._2 ++ edges)
+      case s"Valve $name has flow rate=$rate; tunnel leads to valve $n" =>
+        (acc._1 + (name -> rate.toInt), acc._2 ++ Set((name, n), (n, name)))
+    }
+  )
+
+  def maxFlow(rates: Map[String, Int], graph: Map[String, Set[(String, Int)]]): Int =
+    lazy val maxFlow: ((String, Int, Set[String])) => Int = Util.memoize {
+      case (_, time, _) if time > 30 => 0
+      case (currentNode, time, opened) =>
+        if opened.contains(currentNode) then
+          graph(currentNode).map(n =>
+            maxFlow(n._1, time + n._2, opened)
+          ).max
         else
-          val rem = valves.keys.filter(k => !opened.contains(k)).map(k => valves(k)._1).sum * Math.max(t - time - 1, 0)
-          if (currFlow + rem) < globalMax then
-            currFlow
+          Math.max(
+            rates(currentNode) * Math.max(30 - time - 1, 0) + graph(currentNode).map(n =>
+              maxFlow(n._1, time + n._2 + 1, opened + currentNode)
+            ).max,
+            graph(currentNode).map(n =>
+              maxFlow(n._1, time + n._2, opened)
+            ).max,
+          )
+    }
+    maxFlow("AA", 0, Set())
+
+  def maxFlowE(rates: Map[String, Int], graph: Map[String, Set[(String, Int)]]): Int =
+    val numberWorkingValves = rates.count(_._2 != 0)
+    lazy val maxFlowE: ((String, Int, String, Int, Int, Set[String])) => Int  = Util.memoize {
+      case (_, _, _, _, _, opened) if opened.size == numberWorkingValves => 0
+      case (_, _, _, _, time, _) if time > 26 => 0
+      case (_, _, _, _, time, opened) if (numberWorkingValves - opened.size) > (26 - time) => 0
+      case (m, nextM, e, nextE, time, opened) =>
+        if time == nextM then
+          if opened.contains(m) then
+            graph(m).map(n =>
+              maxFlowE(n._1, time + n._2, e, nextE, Math.min(time + n._2, nextE), opened)
+            ).max
           else
-            Math.max(valves(curr)._2.map(n => maxReleaseH(n, currFlow, opened, time + 1)).max, valves(curr)._2.map(n => maxReleaseH(n,
-              currFlow + valves(curr)._1 * Math.max(t - time - 1, 0), opened + curr, Math.min(t, time + 2))).max
+            Math.max(
+              rates(m) * Math.max(26 - time - 1, 0) + graph(m).map(n =>
+                maxFlowE(n._1, time + n._2 + 1, e, nextE, time, opened + m)
+              ).max,
+              graph(m).map(n =>
+                maxFlowE(n._1, time + n._2, e, nextE, Math.min(time + n._2, nextE), opened)
+              ).max
             )
-    }
-    maxReleaseH("AA", 0, Set(), 0)
-
-  def maxReleaseElephant(valves: Map[String, (Int, List[String])]): Int =
-    var globalMax = 0
-    val t = 26
-    lazy val maxReleaseElephantH: ((String, String, Int, Set[String], Int)) => Int = Util.memoize {
-      case (m, e, currFlow, opened, time) =>
-        if time == t then
-          if currFlow > globalMax then
-            globalMax = currFlow
-          currFlow
-        else if opened.contains(m) || valves(m)._1 == 0 then
-          if opened.contains(e) || valves(e)._1 == 0 then
-            val rem = valves.keys.filter(k => !opened.contains(k)).map(k => valves(k)._1).sum * Math.max(t - time - 1, 0)
-            if (currFlow + rem) < globalMax then
-              currFlow
-            else
-              valves(m)._2.flatMap(nm => valves(e)._2.map(ne => maxReleaseElephantH(nm, ne, currFlow, opened, time + 1))).max
+        else if time == nextE then
+          if opened.contains(e) then
+            graph(e).map(n =>
+              maxFlowE(m, nextM, n._1, time + n._2, Math.min(time + n._2, nextM), opened)
+            ).max
           else
-            0
+            Math.max(
+              rates(e) * Math.max(26 - time - 1, 0) + graph(e).map(n =>
+                maxFlowE(m, nextM, n._1, time + n._2 + 1, time, opened + e)
+              ).max,
+              graph(e).map(n =>
+                maxFlowE(m, nextM, n._1, time + n._2, Math.min(time + n._2, nextM), opened)
+              ).max
+            )
         else
-          0
-
-
-
-
+          maxFlowE(m ,nextM, e, nextE, Math.min(nextM, nextE), opened)
     }
-    maxReleaseElephantH("AA", "AA", 0, Set(), 0)
+    maxFlowE("AA", 0, "AA", 0, 0, Set())
