616952741d02c6b693650b3366ed68562a072c02
Michael Reitgruber <reitgruber.michael@gmail.com>
2022-12-17 22:14:55 +0100

    Day 16

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -4,26 +4,14 @@ import scala.annotation.tailrec
 
 object Day16 extends Day(16):
   override def solve(): Unit =
-    val testInput = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-                      |Valve BB has flow rate=13; tunnels lead to valves CC, AA
-                      |Valve CC has flow rate=2; tunnels lead to valves DD, BB
-                      |Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-                      |Valve EE has flow rate=3; tunnels lead to valves FF, DD
-                      |Valve FF has flow rate=0; tunnels lead to valves EE, GG
-                      |Valve GG has flow rate=0; tunnels lead to valves FF, HH
-                      |Valve HH has flow rate=22; tunnel leads to valve GG
-                      |Valve II has flow rate=0; tunnels lead to valves AA, JJ
-                      |Valve JJ has flow rate=21; tunnel leads to valve II""".stripMargin.split("\n").map(_.strip).toList
-
-    val (rates, edges) = parse(testInput)
+    val (rates, edges) = parse(inputLines)
     val graph = simplify(rates, edges.groupMap(_._1)(_._2))
 
-    //println(maxFlow(rates, graph))
+    //Part 1
+    println(maxFlow(rates, graph))
 
-    Util.time {
-      println(maxFlowE(rates, graph))
-    }
-
+    //Part 2
+    println(maxFlowE(rates, graph))
 
   def simplify(rates: Map[String, Int], edges: Map[String, Set[String]]): Map[String, Set[(String, Int)]] =
     val relevantStarts = rates.filter(k => k._1 == "AA" || k._2 != 0)
@@ -64,41 +52,28 @@ object Day16 extends Day(16):
     maxFlow("AA", 0, Set())
 
   def maxFlowE(rates: Map[String, Int], graph: Map[String, Set[(String, Int)]]): Int =
-    val numberWorkingValves = rates.count(_._2 != 0)
-    lazy val maxFlowE: ((String, Int, String, Int, Int, Set[String])) => Int  = Util.memoize {
-      case (_, _, _, _, _, opened) if opened.size == numberWorkingValves => 0
-      case (_, _, _, _, time, _) if time > 26 => 0
-      case (_, _, _, _, time, opened) if (numberWorkingValves - opened.size) > (26 - time) => 0
-      case (m, nextM, e, nextE, time, opened) =>
-        if time == nextM then
-          if opened.contains(m) then
-            graph(m).map(n =>
-              maxFlowE(n._1, time + n._2, e, nextE, Math.min(time + n._2, nextE), opened)
-            ).max
-          else
-            Math.max(
-              rates(m) * Math.max(26 - time - 1, 0) + graph(m).map(n =>
-                maxFlowE(n._1, time + n._2 + 1, e, nextE, time, opened + m)
-              ).max,
-              graph(m).map(n =>
-                maxFlowE(n._1, time + n._2, e, nextE, Math.min(time + n._2, nextE), opened)
-              ).max
-            )
-        else if time == nextE then
-          if opened.contains(e) then
-            graph(e).map(n =>
-              maxFlowE(m, nextM, n._1, time + n._2, Math.min(time + n._2, nextM), opened)
-            ).max
-          else
-            Math.max(
-              rates(e) * Math.max(26 - time - 1, 0) + graph(e).map(n =>
-                maxFlowE(m, nextM, n._1, time + n._2 + 1, time, opened + e)
-              ).max,
-              graph(e).map(n =>
-                maxFlowE(m, nextM, n._1, time + n._2, Math.min(time + n._2, nextM), opened)
-              ).max
-            )
+    lazy val maxFlowE: ((String, Int, String, Int, Set[String])) => Int  = Util.memoize {
+      case (_, _, _, _, unopened) if unopened.isEmpty => 0
+      case (_, nextM, _, nextE, _) if Math.min(nextM, nextE) > 26 => 0
+      case (_, nextM, _, nextE, unopened) if (26 - Math.min(nextM, nextE)) < unopened.size - (if nextM == nextE then 2 else 1) => 0
+      case (m, _, e, _, unopened) if !unopened.contains(m) || !unopened.contains(e) => 0
+      case (m, nextM, e, nextE, _) if m == e && nextM > nextE => 0
+      case (m, nextM, e, nextE, unopened) =>
+        if nextM <= nextE then
+            rates(m) * Math.max(26 - nextM - 1, 0) + (unopened - m).toList.sortBy(k => rates(k)).reverse.map(n =>
+              val d = graph(m).find(_._1 == n).get._2
+              maxFlowE(n, nextM + d + 1, e, nextE, unopened - m)
+            ).maxOption.getOrElse(0)
+        else if nextE <= nextM then
+            rates(e) * Math.max(26 - nextE - 1, 0) + (unopened - e).toList.sortBy(k => rates(k)).reverse.map(n =>
+              val d = graph(e).find(_._1 == n).get._2
+              maxFlowE(m, nextM, n, nextE + d + 1, unopened - e)
+            ).maxOption.getOrElse(0)
         else
-          maxFlowE(m ,nextM, e, nextE, Math.min(nextM, nextE), opened)
+          maxFlowE(m ,nextM, e, nextE, unopened)
     }
-    maxFlowE("AA", 0, "AA", 0, 0, Set())
+    val next = graph("AA").toList.sortBy(_._2)
+
+    next.flatMap(m => next.dropWhile(_._1 != m._1).map(e => (m ,e))).filter(k => k._1._1 != k._2._1).foldLeft(0)((acc, x) =>
+      Math.max(maxFlowE(x._1._1, x._1._2, x._2._1, x._2._2, rates.filter(_._2 != 0).keys.toSet), acc)
+    )
