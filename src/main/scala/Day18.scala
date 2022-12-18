d9ecccee0cb77cc126a5b05371599ab8f57901fa
Michael Reitgruber <reitgruber.michael@gmail.com>
2022-12-18 19:52:41 +0100

    Day 18

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -9,9 +9,10 @@ object Day18 extends Day(18):
     //Part 1
     println(scan.map(c => neighbors(c).count(n => !scan.contains(n))).sum)
 
-    //Part 2
-    println(scan.flatMap(c => neighbors(c).toList).count(n => !scan.contains(n) && !trapped(scan.toSet, n)))
+    val exterior = reachable(scan.toSet)
 
+    //Part 2
+    println(scan.flatMap(c => neighbors(c)).count(c => exterior.contains(c)))
 
   def neighbors(pos: (Int, Int, Int)): Set[(Int, Int, Int)] =
     (for
@@ -21,21 +22,19 @@ object Day18 extends Day(18):
       if (Math.abs(x) + Math.abs(y) + Math.abs(z)) == 1
     yield (pos._1 + x, pos._2 + y, pos._3 + z)).toSet
 
-  def trapped(scan: Set[(Int, Int, Int)], pos: (Int, Int, Int)): Boolean =
+  def reachable(scan: Set[(Int, Int, Int)]): Set[(Int, Int, Int)] =
     val maxX = scan.maxBy(_._1)._1
     val maxY = scan.maxBy(_._2)._2
     val maxZ = scan.maxBy(_._3)._3
+
     @tailrec
-    def trappedH(curr: Set[(Int, Int, Int)], found: Set[(Int, Int, Int)]): Boolean =
+    def reachableH(curr: Set[(Int, Int, Int)], found: Set[(Int, Int, Int)]): Set[(Int, Int, Int)] =
       val next = curr.flatMap(neighbors).filter(!found.contains(_)).filter(c =>
         !scan.contains(c) && c._1 >= -1 && c._2 >= -1 && c._3 >= -1 && c._1 <= maxX + 1 && c._2 <= maxY + 1 && c._3 <= maxZ + 1
       )
-
       if next.isEmpty then
-        true
-      else if next.contains((0, 0, 0)) then
-        false
+        found
       else
-        trappedH(next, found ++ curr)
-    trappedH(Set(pos), Set())
+        reachableH(next, found ++ curr)
 
+    reachableH(Set((-1, -1, -1)), Set())
